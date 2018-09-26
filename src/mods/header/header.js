2b48ff2dbcd514e2c29a044b2b3dfb0fc5eaef48
TiTitoStart <you@example.com>
2018-09-26 14:37:26 +0800

    完成职业路径页面

---
+++ src/mods/header/header.js
@@ -0,0 +1,50 @@
+window.onload = function () {
+    //给所有头部banner添加click事件
+    for (let i = 0; i < header_as.length; i++) {
+        header_as[i].onclick = function () {
+            console.log(i);
+            headerBannerClick(this.innerHTML);
+            this.classList.add('special-a');
+            this.parentNode.classList.add('bottom-line');
+            for(let j=0;j<header_as.length;j++){
+                if(j!=i){
+                    header_as[j].classList.remove('special-a');
+                    header_as[j].parentNode.classList.remove('bottom-line');
+                }
+            }
+        }
+    }
+}
+var main_page = document.getElementById('main-page');
+var freeClass_page = document.getElementById('freeClass-page');
+var careePath_page = document.getElementById('careePath-page');
+var realFight_page = document.getElementById('realFight-page');
+
+//获取头部banner
+var header_as = document.getElementById('header').querySelectorAll('a');
+var header_spans = document.getElementById('header').querySelectorAll('span');
+var header_right = document.getElementById('header').querySelector('.header-right');
+
+function headerBannerClick(str) {
+    switch (str) {
+        case '首页': main_page.style.display = '';
+            freeClass_page.style.display = 'none';
+            careePath_page.style.display = 'none';
+            realFight_page.style.display = 'none';
+            break;
+        case '免费课程': freeClass_page.style.display = '';
+            main_page.style.display = 'none';
+            careePath_page.style.display = 'none';
+            realFight_page.style.display = 'none';
+            break;
+        case '职业路径': careePath_page.style.display = '';
+            main_page.style.display = 'none';
+            freeClass_page.style.display = 'none';
+            realFight_page.style.display = 'none';
+            break;
+        case '实战课程': realFight_page.style.display = '';
+            main_page.style.display = 'none';
+            freeClass_page.style.display = 'none';
+            careePath_page.style.display = 'none'; break;
+    }
+}
